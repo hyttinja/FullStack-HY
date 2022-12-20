994a1bb82ad70bdc2b1f0ccb091c4cbb2c47ccce
Janne <jannehyttinen1@gmail.com>
2022-12-20 19:37:12 +0200

    osa2: 2.1-2.5

---
+++ osa2/kurssitiedot/src/Course.js
@@ -0,0 +1,31 @@
+const Header = ({ course }) => <h2>{course}</h2>
+
+const Total = ({ sum }) => <p>Total of {sum} exercises</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part =>{
+        return (
+            <Part
+                key={part.id}
+                part={part} 
+            />
+        )
+    })}
+  </>
+const Course = ({course}) =>{
+    return(    
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total sum={course.parts.reduce((sum,part) => sum+part.exercises,0)} />
+        </div>)
+
+}
+
+export default Course;
